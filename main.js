1fa89d69abff6d343d6a84803d38d7b8a7eda544
wai wai <waiwaiphyochun@gmail.com>
2024-10-29 15:35:35 +0800

    update to array object

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const startBtn = document.getElementById("startBtn");
 const fruitBtn = document.getElementById("fruitBtn");
-const animalBtn = document.getElementById("animalBtn");
+const answerListsDiv = document.getElementById("answerLists");
 
 //Start Button
 const startBtnClick = () => {
@@ -11,9 +11,21 @@ startBtn.addEventListener("click", startBtnClick);
 
 //For fruit
 const fruitQuestion = () => {
-  document.getElementById("appleAns").style.display = "block";
+  document.getElementById("appleContainer").style.display = "block";
   document.getElementById("fruitBtn").style.display = "none";
+
+  const answers = [
+    { name: 'Apple'},
+    { name: 'Orange'},
+    { name: 'Peach'}
+  ];
+
+  answerListsDiv.innerHTML = '';
+
+  answers.forEach(answers => {
+    const button = document.createElement('button');
+    button.innerText = answers.name;
+    answerListsDiv.appendChild(button);
+  })
 };
 fruitBtn.addEventListener("click", fruitQuestion);
-
-
