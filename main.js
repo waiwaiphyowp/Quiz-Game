7b5e171c4a45eebb6cda21644001619ae5f26a08
wai wai <waiwaiphyochun@gmail.com>
2024-11-01 22:13:23 +0800

    combine div & update array

--- main.js
+++ main.js
@@ -1,55 +1,44 @@
 const startBtn = document.getElementById("startBtn");
 const fruitBtn = document.getElementById("fruitBtn");
-const answerListsDiv = document.getElementById("answerLists");
-const changeBtn = document.getElementById("changeBtn");
-const animalListsDiv = document.getElementById("animalLists");
+const animalBtn = document.getElementById("animalBtn");
 
-//Start Button
-const startBtnClick = () => {
-  document.getElementById("startBtn").style.display = "none";
+// question & answer
+const questions = {
+  fruit: 
+  {
+    title: "What is this fruit?",
+    image: "./photo/fruit.jpeg",
+    answers: [
+      { name: 'Apple', correct: true},
+      { name: 'Orange', correct: false},
+      { name: 'Peach', correct: false}
+    ]
+  }, 
+
+  animal: {
+    title: "Choose the animal",
+    image: ".photo/rabbit.jepg",
+    answer: [
+      { name: 'Tiger', correct: false},
+      { name: 'Rabbit', correct: true},
+      { name: 'Rat', correct: false}
+    ]
+  }
+};
+
+// start 
+startBtn.addEventListener("click", () => {
+  startBtn.style.display = "none";
   document.getElementById("category").style.display = "block";
-}
-startBtn.addEventListener("click", startBtnClick);
+});
 
-// Change Question & show animal question
-const changeBtnClick = () => {
-  document.getElementById("changeBtn").style.display = "block";
-  document.getElementById("animalContainer").style.display = "block";
-  document.getElementById("appleContainer").style.display = "none";
+// fruit
+fruitBtn.addEventListener("click", () => {
+  displayQuestion('fruit');
+});
 
-  const animalAnswers = [
-    { name: 'Tiger'},
-    { name: 'Rabbit'},
-    { name: 'Rat'}
-  ];
-  animalListsDiv.innerHTML = '';
-
-  animalAnswers.forEach(animalAnswers => {
-    const button = document.createElement('button');
-    button.innerText = animalAnswers.name;
-    animalListsDiv.appendChild(button);
-  });
-};
-changeBtn.addEventListener("click", changeBtnClick);
-
-//For fruit
-const fruitQuestion = () => {
-  document.getElementById("appleContainer").style.display = "block";
-  document.getElementById("fruitBtn").style.display = "none";
-
-  const answers = [
-    { name: 'Apple'},
-    { name: 'Orange'},
-    { name: 'Peach'}
-  ]
-
-  answerListsDiv.innerHTML = '';
-
-  answers.forEach(answers => {
-    const button = document.createElement('button');
-    button.innerText = answers.name;
-    answerListsDiv.appendChild(button);
-  });
-};
-fruitBtn.addEventListener("click", fruitQuestion);
+// animal
+animalBtn.addEventListener("click", () => {
+  displayQuestion('animal'); 
+});
 
