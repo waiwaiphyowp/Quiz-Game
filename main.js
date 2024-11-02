dd448c6f1c8b4b9541ec2d4c0b7bbe11f83ea022
wai wai <waiwaiphyochun@gmail.com>
2024-11-02 21:20:41 +0800

    update UI Design

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const startBtn = document.getElementById("startBtn");
 const fruitBtn = document.getElementById("fruitBtn");
 const animalBtn = document.getElementById("animalBtn");
+const countryBtn = document.getElementById("countryBtn");
 const questionTitle = document.getElementById("questionTitle");
 const answerLists = document.getElementById("answerLists");
 const message = document.getElementById("message");
@@ -8,27 +9,39 @@ const errorMessage = document.getElementById("errorMessage");
 
 // question & answer
 const questions = {
-  fruit: 
-  {
-    title: "What is this fruit?",
+  fruit: {
+    title: "Guess the Fruit",
     image: "./photo/fruit.jpeg",
     answers: [
       { name: 'Apple', correct: true},
       { name: 'Orange', correct: false},
       { name: 'Peach', correct: false}
-    ]
-  }, 
-
+    ],
+  }, // fruit question closing
+  
   animal: {
-    title: "Choose the correct animal.",
+    title: "Guess the Animal",
     image: "./photo/rabbit.jpeg",
     answers: [
       { name: 'Tiger', correct: false},
       { name: 'Rabbit', correct: true},
       { name: 'Rat', correct: false}
     ]
-  }
-};
+  }, // animal question closing
+
+  country: {
+    title: "Guess the Country",
+    image: "./photo/singapore.jpg",
+    answers: [
+      { name: 'Singapore', correct: true},
+      { name: 'Malaysia', correct: false},
+      { name: 'Japan', correct: false}
+    ]
+  }, //country closing
+
+}; // main question closing
+
+
 
 // start 
 startBtn.addEventListener("click", () => {
@@ -46,6 +59,11 @@ animalBtn.addEventListener("click", () => {
   displayQuestion('animal'); 
 });
 
+// country
+countryBtn.addEventListener("click", () => {
+  displayQuestion('country');
+})
+
 //for question & answer
 const displayQuestion = (questionIndex) => {
   const currentQuestion = questions[questionIndex];
@@ -54,14 +72,14 @@ const displayQuestion = (questionIndex) => {
   answerLists.innerHTML = '';
   message.innerHTML = '';
   errorMessage.innerHTML = '';
-
+  
   currentQuestion.answers.forEach(answer => {
     const button = document.createElement('button');
     button.innerText = answer.name;
     button.onclick = () => {
       if(answer.correct) {
-        message.innerText = "Correct!";
-      } else {
+        message.innerText = "Correct!"
+        } else {
         errorMessage.innerText = "Incorrect! Try again.";
       }
     };
